0ed884e34f583f2e78aaf3a968bb088ce82f6f14
mksilva.sfd <maiksilva.la@gmail.com>
2022-03-25 21:04:17 -0300

    studying.py

---
+++ studying.py
@@ -0,0 +1,18 @@
+from asyncore import loop
+
+
+x, y, z = "Maik", "Gabriela", "Lindos"                         #this is for different variables
+
+print (x, y, z) 
+ 
+x = y = z = "Orange"                                           #this is for multiple variables with same value
+
+print (x, y, z)
+
+
+games = ["Lol", "Wow", "Ow"]                                   #Unpack a list or an tuple
+x, y, z = games
+
+print (x, y, z)
+
+
